c193ea970e4990edaec3ef9d2a8b0207d4401dc2
Multipixelone <finn@cnwr.net>
2025-04-27 19:42:33 -0400

    add some test code

--- scripts/subway-run.py
+++ scripts/subway-run.py
@@ -1,5 +1,20 @@
 from nyct_gtfs import NYCTFeed
 
-feed = NYCTFeed("C")
+feed = NYCTFeed("A")
 
-print(feed.filter_trips(line_id=["C"]))
+incoming_trains = set()
+
+trains = feed.filter_trips(line_id=["C"], headed_for_stop_id=["A47N"])
+
+for train in trains:
+    if train.stop_time_updates[0].stop_id == "A47N":
+        incoming_trains.add((train.location_status, train.direction, train.arrival))
+    print(incoming_trains)
+
+
+# print(str(trains[0]))
+
+
+# print(trains[0].stop_time_updates[0].stop_name)
+
+# print(trains[0].stop_time_updates[0].arrival)
