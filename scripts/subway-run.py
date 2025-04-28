3a9f55c9b4394bea8a5813b5e237f23d0a46ab72
Multipixelone <finn@cnwr.net>
2025-04-28 09:05:39 -0400

    add variables for stop and walking time

--- scripts/subway-run.py
+++ scripts/subway-run.py
@@ -1,20 +1,9 @@
 from nyct_gtfs import NYCTFeed
 
 feed = NYCTFeed("A")
+my_stop = ["A47N", "A47S"]
+walking_time = 5
 
 incoming_trains = set()
 
-trains = feed.filter_trips(line_id=["C"], headed_for_stop_id=["A47N"])
-
-for train in trains:
-    if train.stop_time_updates[0].stop_id == "A47N":
-        incoming_trains.add((train.location_status, train.direction, train.arrival))
-    print(incoming_trains)
-
-
-# print(str(trains[0]))
-
-
-# print(trains[0].stop_time_updates[0].stop_name)
-
-# print(trains[0].stop_time_updates[0].arrival)
+trains = feed.filter_trips(line_id=["C"], headed_for_stop_id=my_stop)
